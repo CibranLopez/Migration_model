d2a445bba975718da0ad2cb760d874b93da49ecc
CibranLopez <49532916+CibranLopez@users.noreply.github.com>
2022-01-09 15:25:12 +0100

    Update crossover_functions.h

--- crossover_functions.h
+++ crossover_functions.h
@@ -2,8 +2,8 @@
 //  crossover_functions.h
 //  Migration model
 //
-//  Created by Cibrán López Álvarez on 12/12/2021.
-//
+//  Created by Cibrán López Álvarez on 12/12/2021
+//  Definition of the funcitons for crossover and mutation
 
 
 #ifndef crossover_functions_h
@@ -15,46 +15,47 @@
 
 void one_point_crossover(unsigned long int p1, unsigned long int p2, unsigned long int *f1, unsigned long int *f2, int length) {
     unsigned char d = uniform() * (length - 1) + 1;
-    unsigned long int mask = 0xFFFFFFFFFFFFFFFFU << d;
+    unsigned long int mask = 0xFFFFFFFFFFFFFFFFUL << d;
     
     *f1 = (p1 & mask) | (p2 & ~mask);
     *f2 = (p2 & mask) | (p1 & ~mask);
 }
 
-void crossover(population_structure population[], unsigned int pos_1, unsigned int pos_2, unsigned int i) {
-    one_point_crossover(population[pos_1].x_0, population[pos_2].x_0, &population[i].x_0, &population[i+1].x_0, x_0_length);
-    one_point_crossover(population[pos_1].phi, population[pos_2].phi, &population[i].phi, &population[i+1].phi, phi_length);
-    one_point_crossover(population[pos_1].lambda, population[pos_2].lambda, &population[i].lambda, &population[i+1].lambda, lambda_length);
-    one_point_crossover(population[pos_1].mu, population[pos_2].mu, &population[i].mu, &population[i+1].mu, mu_length);
-    one_point_crossover(population[pos_1].sigma, population[pos_2].sigma, &population[i].sigma, &population[i+1].sigma, sigma_length);
-    one_point_crossover(population[pos_1].delta, population[pos_2].delta, &population[i].delta, &population[i+1].delta, delta_length);
-}
-
 unsigned int get_index(void) {
+    /* The crossover_probability percent takes more changes to be crossed over */
     if (uniform() < crossover_probability)
         return uniform() * (1 - crossover_probability) * N_population;
     else
         return ((1 - crossover_probability) + uniform() * crossover_probability) * N_population;
 }
 
-void make_crossover(population_structure population[], unsigned int i) {
+void make_crossover(population_structure population[], population_structure new_population[], unsigned int i) {
+    /* Getting the indexes and applying crossover to each parameter */
     unsigned int index_1 = get_index(), index_2 = get_index();
-    crossover(population, index_1, index_2, i);
+    
+    one_point_crossover(population[index_1].x_0, population[index_2].x_0, &new_population[i].x_0, &new_population[i+1].x_0, x_0_length);
+    one_point_crossover(population[index_1].phi, population[index_2].phi, &new_population[i].phi, &new_population[i+1].phi, phi_length);
+    one_point_crossover(population[index_1].lambda, population[index_2].lambda, &new_population[i].lambda, &new_population[i+1].lambda, lambda_length);
+    one_point_crossover(population[index_1].mu, population[index_2].mu, &new_population[i].mu, &new_population[i+1].mu, mu_length);
+    one_point_crossover(population[index_1].sigma, population[index_2].sigma, &new_population[i].sigma, &new_population[i+1].sigma, sigma_length);
+    one_point_crossover(population[index_1].delta, population[index_2].delta, &new_population[i].delta, &new_population[i+1].delta, delta_length);
 }
 
-void mutation(unsigned long int *f, int length) {
+void one_point_mutation(unsigned long int *f, int length) {
+    /* Randomly mutating a position in binary representation */
     for (int i = 0; i < length; i++)
-        if (uniform() < mutation_probability)
-            *f = (*f)^(1U << i);
+    if (uniform() < mutation_probability)
+        *f = (*f)^(1UL << (unsigned char) uniform() * length);
 }
 
-void mutate(population_structure population[], unsigned int i) {    
-    mutation(&population[i].x_0, x_0_length);
-    mutation(&population[i].phi, phi_length);
-    mutation(&population[i].lambda, lambda_length);
-    mutation(&population[i].mu, mu_length);
-    mutation(&population[i].sigma, sigma_length);
-    mutation(&population[i].delta, delta_length);
+void mutate(population_structure population[], unsigned int i) {
+    /* Aapplying mutation to each parameter */
+    one_point_mutation(&population[i].x_0, x_0_length);
+    one_point_mutation(&population[i].phi, phi_length);
+    one_point_mutation(&population[i].lambda, lambda_length);
+    one_point_mutation(&population[i].mu, mu_length);
+    one_point_mutation(&population[i].sigma, sigma_length);
+    one_point_mutation(&population[i].delta, delta_length);
 }
 
 
